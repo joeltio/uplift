0e7eb9f5dec2aaf3838c91015be4bf39d6eb9c2a
Joel Tio <joeltiojinhon@gmail.com>
2017-07-28 20:23:36 +0800

    Add Login page

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import { Link } from "react-router-dom";
 
+import Login from "./Login/Login";
+
 export default class Layout extends React.Component {
     render() {
-        const Navs = [];
+        const Navs = [
+            ["Login", "/"]
+        ];
 
         return (
-            <div></div>
+            <div>
+                <Route exact path="/" component={Login}></Route>
+            </div>
         );
     }
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CSSModules from "react-css-modules";
+
+import styles from "./login.css";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default CSSModules(Login, styles);
