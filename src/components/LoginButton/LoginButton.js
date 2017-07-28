07034121cfd50a4f8df6d7af151c84436822f6c0
Joel Tio <joeltiojinhon@gmail.com>
2017-07-29 07:46:23 +0800

    Add react router to LoginButton

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,12 +1,15 @@
 import React from "react";
 import CSSModules from "react-css-modules";
+import { Link } from "react-router-dom";
 
 import styles from "./loginButton.css";
 
 class LoginButton extends React.Component {
     render() {
         return (
-            <button styleName="button">{this.props.children}</button>
+            <Link to={this.props.linkTo}>
+                <button styleName="button">{this.props.children}</button>
+            </Link>
         );
     }
 }
