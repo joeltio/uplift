c3f23023dcd587d4c62565b7d22072a6b1bbe440
Joel Tio <joeltiojinhon@gmail.com>
2017-07-29 07:22:58 +0800

    Add Connect page styling and sample text

--- src/pages/Connect/Connect.js
+++ src/pages/Connect/Connect.js
@@ -3,10 +3,24 @@ import CSSModules from "react-css-modules";
 
 import styles from "./connect.css";
 
+import ProfileTestimonial from "../../components/ProfileTestimonial/ProfileTestimonial";
+import RoundButton from "../../components/RoundButton/RoundButton";
+
 class Connect extends React.Component {
     render() {
+        const name = "Nick Halden";
+        const testimony = "\"Nick is the best therapist I have been to. He really understands me and helps me through difficult times.\"";
         return (
-            <h1>Hello</h1>
+            <div styleName="connect-page">
+                <h1 styleName="header">Your therapist is waiting to chat with you now</h1>
+                <div styleName="content">
+                    <ProfileTestimonial
+                        name={name}
+                        testimony={testimony} />
+                        <p></p>
+                    <RoundButton>Talk to the therapist now</RoundButton>
+                </div>
+            </div>
         );
     }
 }
